57710f04bda48fea2a0d7f66cd559179c764cb4d
avara1986 <a.vara.1986@gmail.com>
2017-02-20 20:36:48 +0100

    Added autotag script as alias to command line

--- setup.py
+++ setup.py
@@ -1,13 +1,58 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2016 by Alberto Vara <a.vara.1986@gmail.com>
-
-
+from __future__ import absolute_import
 import os
+import sys
+import site
 from setuptools import setup, find_packages
+from distutils.command.install import install
 
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+def binaries_directory():
+    """Return the installation directory, or None"""
+    if '--user' in sys.argv:
+        paths = (site.getusersitepackages(),)
+    else:
+        py_version = '%s.%s' % (sys.version_info[0], sys.version_info[1])
+        paths = (s % (py_version) for s in (
+            sys.prefix + '/lib/python%s/dist-packages/',
+            sys.prefix + '/lib/python%s/site-packages/',
+            sys.prefix + '/local/lib/python%s/dist-packages/',
+            sys.prefix + '/local/lib/python%s/site-packages/',
+            '/Library/Python/%s/site-packages/',
+        ))
+
+    for path in paths:
+        if os.path.exists(path):
+            return path
+    print('no installation path found', sys.stderr)
+    return None
+
+
+def add_script_as_command(command, *args):
+    path = binaries_directory()
+    command_to_write = command + "=" + os.path.join(path, "avython", *args)
+    alias_line = "alias " + command_to_write
+    command_line = command_to_write
+    bashrc_path = os.path.expanduser('~/.bashrc')
+    with open(bashrc_path, 'r+') as file:
+        bashrc_text = file.read()
+        if alias_line not in bashrc_text:
+            file.write('\n{}\n'.format(alias_line))
+        if command_line not in bashrc_text:
+            file.write('\n{}\n'.format(command_line))
+
+
+class post_install(install):
+
+    def run(self):
+        install.run(self)
+
+        add_script_as_command("autotag", "gitautotag", "main.py")
 
 
 setup(
@@ -30,9 +75,10 @@ setup(
     license="MIT",
     platforms=["any"],
     keywords="avython",
-    url='https://github.com/avara1986/python-utils.git',
+    url='https://github.com/avara1986/avython.git',
     test_suite='nose.collector',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
+    cmdclass={'install': post_install},
 )
